c5113eb6268448a368b24222c01cd85477ff5b9a
Quaranta Arthur <arthur.quaranta@ynov.com>
2021-11-27 14:10:44 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'arthurIonicProject',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
